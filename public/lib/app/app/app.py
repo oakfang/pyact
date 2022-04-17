3042a05e033e1d91ab68fe3bb9781b1ad7fc8f3c
Alon Niv <alon.niv@taboola.com>
2022-04-17 09:48:56 +0300

    add dark theme

--- public/lib/app/app/app.py
+++ public/lib/app/app/app.py
@@ -1,8 +1,13 @@
-from pyact import component
+from pyact import component, as_js
+from mui import createTheme
 from .node_modules import mui
 from .home import home
 
+theme = createTheme(as_js({"palette": {"mode": "dark"}}))
+
+
 @component
 def app():
-    +mui.CssBaseline()
-    +home()
+    with mui.ThemeProvider(theme=theme):
+        +mui.CssBaseline()
+        +home()
